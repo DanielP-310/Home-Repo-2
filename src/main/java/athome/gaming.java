a0b549035c9dac1c801cf38b5b9bcff812259d1d
DanielP-310 <dporter283@lsr7.net>
2025-10-03 20:24:43 -0500

    It's Alive
    I got maven to exist, not sure about pom.xml

---
+++ src/main/java/athome/gaming.java
@@ -0,0 +1,9 @@
+package main.java.athome;
+
+public class gaming {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hi chat");
+    }
+}
---
+++ src/main/java/athome/javafx/App.java
@@ -0,0 +1,19 @@
+package main.java.athome.javafx;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    private static Scene scene;
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hi chat");
+    }
+}
